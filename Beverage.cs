7de49ba6d2e068b24a42f3f30ac94b455262b14a
Swastika-App <swastgupta@microsoft.com>
2024-08-20 22:40:37 +0530

    Update Beverage.cs

--- Beverage.cs
+++ Beverage.cs
@@ -2,16 +2,14 @@
 
 namespace VendingMachineSimulation
 {
-    // Beverage class inheriting from the abstract Product class
+    // inheritance from abstract product class
     public class Beverage : Product
     {
-        // Constructor to set the name of the product to "Beverage"
         public Beverage()
         {
             Name = "Beverage";
         }
 
-        // Implementation of the abstract Dispense method
         public override void Dispense()
         {
             Console.WriteLine("Dispensing a beverage...");
