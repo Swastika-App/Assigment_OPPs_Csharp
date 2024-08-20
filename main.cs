db3a09ef32dd2a0960f16f36dc9fd24b655a4d10
Swastika-App <swastgupta@microsoft.com>
2024-08-20 21:35:36 +0530

    vending machine main class

---
+++ main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace VendingMachineSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the VendingMachine
+            VendingMachine vendingMachine = new VendingMachine();
+
+            // Create instances of Snack and Beverage
+            Product snack = new Snack();
+            Product beverage = new Beverage();
+
+            // Use the SelectProduct method to select and dispense a snack
+            vendingMachine.SelectProduct(snack);
+
+            // Use the SelectProduct method to select and dispense a beverage
+            vendingMachine.SelectProduct(beverage);
+        }
+    }
+}
